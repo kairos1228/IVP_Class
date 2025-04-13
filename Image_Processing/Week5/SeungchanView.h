7c80a80351e6157824541dd4f1da4c3dace57db4
Seungchan Baek <88337917+kairos1228@users.noreply.github.com>
2025-04-14 01:14:09 +0900

    5주차 실습 파일 업로드

---
+++ Image_Processing/Week5/SeungchanView.h
@@ -0,0 +1,65 @@
+﻿
+// SeungchanView.h: CSeungchanView 클래스의 인터페이스
+//
+
+#pragma once
+
+
+class CSeungchanView : public CView
+{
+protected: // serialization에서만 만들어집니다.
+	CSeungchanView() noexcept;
+	DECLARE_DYNCREATE(CSeungchanView)
+
+// 특성입니다.
+public:
+	CSeungchanDoc* GetDocument() const;
+
+// 작업입니다.
+public:
+
+// 재정의입니다.
+public:
+	virtual void OnDraw(CDC* pDC);  // 이 뷰를 그리기 위해 재정의되었습니다.
+	virtual BOOL PreCreateWindow(CREATESTRUCT& cs);
+protected:
+	virtual BOOL OnPreparePrinting(CPrintInfo* pInfo);
+	virtual void OnBeginPrinting(CDC* pDC, CPrintInfo* pInfo);
+	virtual void OnEndPrinting(CDC* pDC, CPrintInfo* pInfo);
+
+// 구현입니다.
+public:
+	virtual ~CSeungchanView();
+#ifdef _DEBUG
+	virtual void AssertValid() const;
+	virtual void Dump(CDumpContext& dc) const;
+#endif
+
+protected:
+
+// 생성된 메시지 맵 함수
+protected:
+	afx_msg void OnFilePrintPreview();
+	afx_msg void OnRButtonUp(UINT nFlags, CPoint point);
+	afx_msg void OnContextMenu(CWnd* pWnd, CPoint point);
+	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg void OnDownSampling();
+	afx_msg void OnUpSampling();
+	afx_msg void OnQuantization();
+	afx_msg void OnSumConstant();
+	afx_msg void OnSubConstant();
+	afx_msg void OnMulConstant();
+	afx_msg void OnDivConstant();
+	afx_msg void OnAndOperate();
+	afx_msg void OnNegaTransform();
+	afx_msg void OnGammaCorrection();
+	afx_msg void OnBinarization();
+	afx_msg void On32785();
+};
+
+#ifndef _DEBUG  // SeungchanView.cpp의 디버그 버전
+inline CSeungchanDoc* CSeungchanView::GetDocument() const
+   { return reinterpret_cast<CSeungchanDoc*>(m_pDocument); }
+#endif
+
